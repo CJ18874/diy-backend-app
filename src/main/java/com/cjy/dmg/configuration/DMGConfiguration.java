868a2aed527d376b990dba1846f091acacbfec7c
Yanushkevich, Chen <Chen_Yanushkevich2@comcast.com>
2020-03-14 22:16:01 -0400

    Test SPRING_DATASOURCE_PASSWORD

--- src/main/java/com/cjy/dmg/configuration/DMGConfiguration.java
+++ src/main/java/com/cjy/dmg/configuration/DMGConfiguration.java
@@ -1,26 +1,11 @@
 package com.cjy.dmg.configuration;
 
-import java.net.URISyntaxException;
-
-import javax.sql.DataSource;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DMGConfiguration {
-
-//    @Bean
-//    public DataSource dataSource() throws URISyntaxException {
-//    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setUsername("JDBC_DATABASE_USERNAME");
-//		dataSource.setPassword("JDBC_DATABASE_PASSWORD");
-//		dataSource.setUrl("JDBC_DATABASE_URL");
-//		dataSource.setDriverClassName("org.postgresql.Driver");
-//		return dataSource;
-//    }
     
 	@Bean
 	public ModelMapper modelMapper() {

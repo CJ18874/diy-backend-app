0a900c2f49baf2a269dead7612410f5b47869844
Yanushkevich, Chen <Chen_Yanushkevich2@comcast.com>
2020-03-14 21:32:15 -0400

    Update prod

--- src/main/java/com/cjy/dmg/configuration/DMGConfiguration.java
+++ src/main/java/com/cjy/dmg/configuration/DMGConfiguration.java
@@ -1,12 +1,26 @@
 package com.cjy.dmg.configuration;
 
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DMGConfiguration {
 
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("JDBC_DATABASE_USERNAME");
+		dataSource.setPassword("JDBC_DATABASE_PASSWORD");
+		dataSource.setUrl("JDBC_DATABASE_URL");
+		return dataSource;
+    }
+    
 	@Bean
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
